e79c0ef7018c1e8228c3212fbdd1ef0290e16de0
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2025-11-04 00:43:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
