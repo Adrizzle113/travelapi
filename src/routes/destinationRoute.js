1bebd50671f99e9721b68a56bf795e1839da981c
Adrizzle113 <32805246+Adrizzle113@users.noreply.github.com>
2025-12-26 05:03:53 +0000

    fix: Correct destination route path

--- src/routes/destinationRoute.js
+++ src/routes/destinationRoute.js
@@ -2,6 +2,6 @@ import express from "express";
 import destinationController from "../controllers/DestinationController.js";
 const DestinationRoute = express.Router();
 
-DestinationRoute.post("/destination", destinationController);
+DestinationRoute.post("/", destinationController);
 
 export default DestinationRoute;
